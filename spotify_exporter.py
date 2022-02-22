9a2253459e259800faeb8f24d5d85c153ed637ed
Kelli Budd <kelli.budd@floqast.com>
2022-02-21 21:01:06 -0800

    cleanup

--- spotify_exporter.py
+++ spotify_exporter.py
@@ -3,40 +3,28 @@ Using Spotipy to make calls to the Spotify Web API.
 
 Control variables - [All are either global or in main()]
 	* CLI_ID and CLI_KEY	(string)
-	* overwrite 			(boolean)
-	* mode 					(string)
-	* playlist				(list of strings)
 '''
 
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-import random
-from pprint import pprint
-import csv
 import os
 import pandas as pd
 
 
-# client ID and secret key to authorize querying of spotify data through the API
-CLI_ID 	= 'a4514ed21d5c4f6c822d961f08ffbcef' # YOUR CLIENT ID
-CLI_KEY = 'e7fdc773a9c44aa0b14ecb5342bbb3ac' # YOUR CLIENT SECRET
+CLI_ID 	= os.environ.get('CLI_ID') # CLIENT ID 
+CLI_KEY = os.environ.get('CLI_KEY') # CLIENT SECRET 
 
+# header row for tsv file 
+tsv_headers = ["Artist","Album","TrackName", "Label"]
 
-# header row for csv file 
-csv_headers = ["Artist","Album","TrackName", "Label"]
-
-# whether you want to overwrite existing files or not
-OVERWRITE = True
-
-
-def write_playlist(username, uri, mode, cli_id=CLI_ID, cli_key=CLI_KEY, token=None):
+def build_playlist(username, uri, cli_id=CLI_ID, cli_key=CLI_KEY, token=None):
 	'''
 	Query the spotify API and receive the playlist information. If mode is 'nan' you can view this information data structure in its raw form.
 	Obtain the list of tracks from the playlist information data structure and write it to a txt or csv file.
 	Select a random song from the list of tracks and print general information to the console. 
 	'''
 	global spotify
-	playlist_df = pd.DataFrame(columns = csv_headers)
+	playlist_df = pd.DataFrame(columns = tsv_headers)
 	
 	# step 1 - get authorized by the spotify API
 	if token:
@@ -48,14 +36,15 @@ def write_playlist(username, uri, mode, cli_id=CLI_ID, cli_key=CLI_KEY, token=No
 		spotify = spotipy.Spotify(auth_manager=auth_manager)
 	
 	playlist_info = spotify.user_playlist_tracks(username, uri)["items"]					#, fields='tracks,next,name'
-	print('info', type(playlist_info))
 	playlist_name = spotify.user_playlist(username, uri, fields="name")
-	print('name', playlist_name)
 
 	for track in playlist_info:
 		album_id = track["track"]["album"]["uri"]
-		# track_id = track["track"]["id"]  
-		
+
+		# when a track cannot be found, skip to the next track
+		if not album_id:
+			continue
+
 		length = len(album_id) # get length of string
 		
 		# create new string of last 22 characters
@@ -86,46 +75,12 @@ def write_playlist(username, uri, mode, cli_id=CLI_ID, cli_key=CLI_KEY, token=No
 	# 		print("Playlist is empty!")
 	# 		return 
 
-	if mode == 'csv':
-		filename = "{0}.tsv".format(playlist_name['name'])
-		old_total = write_csv(filename, playlist_df, webapp) 
-	else:
-		print("Error -- wrong filetype")
+	filename = "{0}.tsv".format(playlist_name['name'])
+	write_tsv_file(filename, playlist_df, webapp) 
 
 	return filename
 
 
-def write_csv(filename, playlist_df, webapp):
-	'''
-	ADD TO CSV FILE
-	View the playlist information data structure if this is confusing! 
-	Specify the destination file path and check if the file exists already. If the file exists and you selected to not overwrite, the program will end here.
-	Traverse the tracks data structure and add whatever information you want to store to a python list. These are the rows for your csv file
-	Append all of these lists to a main python list which will store all the rows for your csv file.
-	Write the data to the csv file!
-	Exceptions handle the cases where the characters in the track info cannot be understood by the system and where the key is invalid (usually due to local files in the playlist).
-	'''
-	if webapp:
-		parent_path = "Saved Data\\"
-		if not os.path.exists(parent_path):
-			os.makedirs(parent_path)
-		filepath = parent_path + filename
-	else:
-		filepath = "C:\\path\\to\\the\\directory\\{0}".format(filename)
-
-	if os.path.isfile(filepath):
-		print("File already exists!")
-		if not OVERWRITE:
-			return
-		else:
-			print("Rewriting...")
-
-	
-
-    #Step 3
-	
+def write_tsv_file(filename, playlist_df, webapp):
+	# Filename is in tsv format so will download as tsv
 	return playlist_df.to_csv(filename, index=False, sep="\t")
-
-
-if __name__ == "__main__":
-	main()
